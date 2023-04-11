0bbb8cd669b1fd15742037b1a6167a0bfe5e9c6d
Aditi Singh <aditisingh@192.168.1.4>
2023-04-12 00:15:43 +0530

    updating next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      apiKey: '0b79b144df78ddfe4fd71d6df02ab2e0',
+    },
+  }
+  
+  module.exports = nextConfig
+  
